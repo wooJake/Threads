9f9653254ccfb0cd010a735df6cd29f4c4c8e1db
wooJake <167344799+wooJake@users.noreply.github.com>
2024-10-08 16:26:47 -0700

    Update main.c

--- main.c
+++ main.c
@@ -6,6 +6,12 @@
 
 int thread_count;//Number of threads, global.
 
+void *Hello(void *id){
+
+    long thread_id = (long) id;
+    printf("Hello from %id of %d threads \n", thread_id, thread_count);
+    return NULL;
+}
 int main(int argc, char *argv[]){
     
     long t_id;
@@ -18,7 +24,7 @@ int main(int argc, char *argv[]){
     
     for(t_id = 0; t_id < thread_count; ++t_id){
         
-        pthread_create(&thread_handles[t_id], );
+        pthread_create(&thread_handles[t_id], NULL, Hello, (void*) t_id);
     }
     
     return 0;
